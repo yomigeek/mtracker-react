66ffe4fb536a6dfeb900cc029f7c0cda5bec689b
Oyomi <seayomi@gmail.com>
2018-08-27 18:29:35 +0100

    chore(setup-eslint): configure linting - setup eslint for linting
    [Delivers #160063121]

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,14 +1,12 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers/index';
+import { createStore, applyMiddleware } from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import rootReducer from '../reducers/index';
 
 
-const configureStore = (initialState) => {
-    return createStore(
-        rootReducer,
-        initialState,
-        applyMiddleware(reduxImmutableStateInvariant()),
-    );
-};
+const configureStore = initialState => createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(reduxImmutableStateInvariant()),
+);
 
 export default configureStore;
